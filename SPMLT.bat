e2b6c2154595079346f9bce2e7eb4c7f3200a6f7
christoher22112003 <aklexysmuzo33@outlook.com>
2021-10-26 15:09:22 -0500

    Se crea el menu principal.

---
+++ SPMLT.bat
@@ -0,0 +1,41 @@
+@echo off
+
+@REM Para poder realizar la creacion de un lote ne un archivo .bat se utiliza <:nombre_del_lote>
+
+  :menu_principal
+
+  @REM Para la impresion de algun tipo de texto en la pantalla se utiliza en <echo  texto_que_se_desea_imprimir>
+  @REM Tambien se puede utilizar para poder realizar menus de de opciones simples 
+  @REM El arte en el cual esta ilustrada la torre se denomina <ASCII> 
+
+    echo                                   ()
+    echo                                   /\
+    echo                                  I  I
+    echo                                  ====
+    echo                                   XX
+    echo                                  xXXx
+    echo                                  XXXX
+    echo                                  XXXX
+    echo                                  XXXX
+    echo                                 xXXXXx
+    echo                                 XXXXXX
+    echo                                 XXXXXX
+    echo                                xXXXXXXx
+    echo                                XXXXXXXX
+    echo                               xXXXXXXXXx
+    echo                               XXXXXXXXXX
+    echo                              XXXXX  XXXXX
+    echo                             xXXXX"  "XXXXx
+    echo                            XXXXXxxxxxxXXXXX
+    echo                          xXXXXX""""""""XXXXXx
+    echo                       xXXXXXX"        "XXXXXXx
+    echo                     xxXXXXXXX            XXXXXXXxx
+    echo ****************************************************************
+    echo **           Bienvenido al script de envio de tareas           **
+    echo ----------------------------------------------------------------
+    echo ==  Que proceso necesitas realizar dentro del envio de tareas  == 
+    echo ==   1.Escaneo de las tareas                                   ==
+  	echo ==   2.Unir los archivos escaneador                            ==
+	echo ==   3.Envio de las tareas                                     ==
+	echo ==   4.Salir                                                   ==
+    echo ----------------------------------------------------------------
